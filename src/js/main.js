de1b80f3c2210e1b849e54f2afd32d2fcee7b31f
Your Name <you@example.com>
2021-12-10 21:00:48 +0100

    ¨javascript¨

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,39 @@
-'use strict';
+"use strict";
 
-
+//SHARE SECTION (BUTTON COLOR AND COLLAPSABLE)
 //ELEMENTOS HTML
+const shareButton = document.querySelector(".share_button__item");
+const shareCardCreation = document.querySelector(".share_creation");
 
-const sharebutton = document.querySelector(".share_button__item");
-const sharecreation = document.querySelector(".share_creation ");
-
-//SHARE BUTTON FUCNTION
-function changeColor(event) {
-  sharebutton.classList.toggle("sharebuttongrey");
-  sharecreation.classList.toggle("collapsed");
+function createAndShareCard(event) {
+  event.preventDefault();
+  shareButton.classList.toggle("sharebuttongrey");
+  shareCardCreation.classList.toggle("collapsed");
 }
 
-sharebutton.addEventListener("click", changeColor);
+shareButton.addEventListener("click", createAndShareCard);
 
 //Collapsables
 //Revisar collapsable en share
 
-const legends = document.querySelectorAll('.js-legend');
-const designContainer = document.querySelector('.design-container');
-const fillContainer = document.querySelector('.fill-container');
-const shareContainer = document.querySelector('.sharecontainer');
+const legends = document.querySelectorAll(".js-legend");
+const designContainer = document.querySelector(".design-container");
+const fillContainer = document.querySelector(".fill-container");
+const shareContainer = document.querySelector(".sharecontainer");
 
-
-function legendHandler(){
-  designContainer.classList.toggle('collapsed');
+function legendHandler() {
+  designContainer.classList.toggle("collapsed");
 }
 
-function legendHandler1(){
-  fillContainer.classList.toggle('collapsed');
-
+function legendHandler1() {
+  fillContainer.classList.toggle("collapsed");
 }
 
-function legendHandler2(){
+/*function legendHandler2(){
   shareContainer.classList.toggle('collapsed');
 }
+*/
 
- 
-legends[0].addEventListener('click', legendHandler);
-legends[1].addEventListener('click', legendHandler1);
-legends[2].addEventListener('click', legendHandler2);
-
-
+legends[0].addEventListener("click", legendHandler);
+legends[1].addEventListener("click", legendHandler1);
+legends[2].addEventListener("click", legendHandler2);
