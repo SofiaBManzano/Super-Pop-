2cff142efa2d44fbbaa1b5a15656644ca49c1a2d
Your Name <you@example.com>
2021-12-16 11:56:42 +0100

    cardcolors

--- src/js/02palettes.js
+++ src/js/02palettes.js
@@ -1,10 +1,12 @@
-const colorOptions = document.querySelectorAll(".design__radio");
+
 
 // Elementos que cambian
+const colorOptions = document.querySelectorAll(".design__radio");
 const icon = document.querySelectorAll(".icon-js");
 const tinyRectangle = document.querySelector(".rectangle-js");
 const fullName = document.querySelector(".fullname-js");
 
+//Handlefunction
 function handleChangeColorCard(event) {
   console.log(event.target.id);
   const optionSelected = event.target.id;
@@ -12,13 +14,14 @@ function handleChangeColorCard(event) {
   if (optionSelected === "blue-green") {
     tinyRectangle.classList.remove("rectanglered");
     tinyRectangle.classList.remove("rectanglemix");
-    tinyRectangle.classList.add("rectangleblue");
+    tinyRectangle.classList.add("card__title--rectangle");
     fullName.classList.remove("namered");
     fullName.classList.remove("namemix");
-    fullName.classList.add("name")
-      // SECOND OPTION RED-ORANGE RECTANGLE&NAME
+    fullName.classList.add("name");
+
+    // SECOND OPTION RED-ORANGE RECTANGLE&NAME
   } else if (optionSelected === "red-orange") {
-    tinyRectangle.classList.remove("rectangleblue");
+    tinyRectangle.classList.remove("card__title--rectangle");
     tinyRectangle.classList.remove("rectanglemix");
     tinyRectangle.classList.add("rectanglered");
     fullName.classList.remove("namemix");
@@ -27,15 +30,27 @@ function handleChangeColorCard(event) {
 
     // SECOND OPTION COLOR-MIX RECTANGLE&NAME
   } else if (optionSelected === "color-mix") {
-    tinyRectangle.classList.remove("rectangleblue");
+    tinyRectangle.classList.remove("card__title--rectangle");
     tinyRectangle.classList.remove("rectanglered");
     tinyRectangle.classList.add("rectanglemix");
     fullName.classList.remove("namered");
     fullName.classList.remove("name");
     fullName.classList.add("namemix");
-  }
-  for (const eachIcon of icon){ 
-        
+  }//ICON CIRCLES
+  for (const eachIcon of icon) {
+    if (optionSelected === "blue-green") {
+      eachIcon.classList.remove("mixicon");
+      eachIcon.classList.remove("redicon");
+      eachIcon.classList.add("blueicon");
+    } else if (optionSelected === "red-orange") {
+      eachIcon.classList.remove("mixicon");
+      eachIcon.classList.remove("iconblue");
+      eachIcon.classList.add("redicon");
+    } else if (optionSelected === "color-mix") {
+      eachIcon.classList.remove("redicon");
+      eachIcon.classList.remove("iconblue");
+      eachIcon.classList.add("mixicon");
+    }
   }
 }
 
