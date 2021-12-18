7ad0c4cade4eef2c07f8eb81f372f89f499954ee
Your Name <you@example.com>
2021-12-18 22:04:06 +0100

    refactorizacion

--- src/js/02palettes.js
+++ src/js/02palettes.js
@@ -8,49 +8,49 @@ const fullName = document.querySelector(".fullname-js");
 
 //Handlefunction
 function handleChangeColorCard(event) {
-  console.log(event.target.id);
   const optionSelected = event.target.id;
   // FIRST OPTION BLUE-GREEN RECTANGLE&NAME
   if (optionSelected === "blue-green") {
     tinyRectangle.classList.remove("rectanglered");
     tinyRectangle.classList.remove("rectanglemix");
-    tinyRectangle.classList.add("card__title--rectangle");
+    tinyRectangle.classList.add("rectangleblue");
     fullName.classList.remove("namered");
     fullName.classList.remove("namemix");
-    fullName.classList.add("name");
+    fullName.classList.add("nameblue");
 
     // SECOND OPTION RED-ORANGE RECTANGLE&NAME
   } else if (optionSelected === "red-orange") {
-    tinyRectangle.classList.remove("card__title--rectangle");
+    tinyRectangle.classList.remove("rectangleblue");
     tinyRectangle.classList.remove("rectanglemix");
     tinyRectangle.classList.add("rectanglered");
     fullName.classList.remove("namemix");
-    fullName.classList.remove("name");
+    fullName.classList.remove("nameblue");
     fullName.classList.add("namered");
 
     // SECOND OPTION COLOR-MIX RECTANGLE&NAME
   } else if (optionSelected === "color-mix") {
-    tinyRectangle.classList.remove("card__title--rectangle");
+    tinyRectangle.classList.remove("rectangleblue");
     tinyRectangle.classList.remove("rectanglered");
     tinyRectangle.classList.add("rectanglemix");
     fullName.classList.remove("namered");
-    fullName.classList.remove("name");
+    fullName.classList.remove("nameblue");
     fullName.classList.add("namemix");
   }//ICON CIRCLES
   for (const eachIcon of icon) {
     if (optionSelected === "blue-green") {
-      eachIcon.classList.remove("mixicon");
-      eachIcon.classList.remove("redicon");
-      eachIcon.classList.add("blueicon");
+      eachIcon.classList.remove("mixiconcircle");
+      eachIcon.classList.remove("rediconcircle");
+      eachIcon.classList.add("blueiconcircle");
     } else if (optionSelected === "red-orange") {
-      eachIcon.classList.remove("mixicon");
-      eachIcon.classList.remove("iconblue");
-      eachIcon.classList.add("redicon");
+      eachIcon.classList.remove("mixiconcircle");
+      eachIcon.classList.remove("iconbluecircle");
+      eachIcon.classList.add("rediconcircle");
     } else if (optionSelected === "color-mix") {
-      eachIcon.classList.remove("redicon");
-      eachIcon.classList.remove("iconblue");
-      eachIcon.classList.add("mixicon");
-    }
+      eachIcon.classList.remove("rediconcircle");
+      eachIcon.classList.remove("iconbluecircle");
+      eachIcon.classList.add("mixiconcircle");
+    }//ICONS
+    
   }
 }
 
