e1c0d51afda60093c689599a5df7af9a9e9a47d2
SofiaBManzano <sofiabmanzano@gmail.com>
2021-12-26 19:15:04 +0100

    button fixed

--- src/js/01collapsables.js
+++ src/js/01collapsables.js
@@ -1,15 +1,17 @@
-'use strict';
+"use strict";
 
 const legends = document.querySelectorAll(".js-legend");
 
-legends.forEach( legend => legend.addEventListener('click', legendHandler));
+legends.forEach((legend) => legend.addEventListener("click", legendHandler));
 
-function legendHandler(event){
-    const arrow = event.target.children[1];
-    collapse(event);  
-    arrow.classList.toggle('legend__arrow--up');
+function legendHandler(event) {
+  const arrow = event.currentTarget.children[1];
+  collapse(event);
+  arrow.classList.toggle("legend__arrow--up");
 }
 //Esta función nos coge el siguiente elemento hermano y le hace un toggle.
-function collapse(elementToCollapse){
-    elementToCollapse.currentTarget.nextElementSibling.classList.toggle('collapsed');
+function collapse(elementToCollapse) {
+  elementToCollapse.currentTarget.nextElementSibling.classList.toggle(
+    "collapsed"
+  );
 }
--- src/js/04cardLink.js
+++ src/js/04cardLink.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const shareCreationLink = document.querySelector(".js-shareCreationLink");
-const twitterHref = document.querySelector('.share_creation__twitter');
-
-
+const twitterHref = document.querySelector(".share_creation__twitter");
+const messageNewCard = document.querySelector(".share_creation__title");
+const shareCardRrss = document.querySelectorAll(".js-share-RRSS");
 function sendUserInfo() {
   fetch("https://awesome-profile-cards.herokuapp.com/card", {
     method: "POST",
@@ -11,21 +11,25 @@ function sendUserInfo() {
     headers: { "Content-Type": "application/json" },
   })
     .then((response) => response.json())
-    .then((data) => {
-      if (data.success === true) {
-        shareCreationLink.href = data.cardURL;
-        shareCreationLink.innerText = data.cardURL;
-        twitterHref.href = `https://twitter.com/intent/tweet?text=%C2%A1Comparte%20esta%20tarjeta%20super%20molona%21&url=${data.cardURL}`;
-       // linkedinHref.href = `https://www.linkedin.com/sharing/share-offsite/?url=${data.cardURL}`;
+    .then((dataServe) => {
+      console.log("hola");
+      if (dataServe.success === true) {
+        console.log("adios");
+        shareCreationLink.href = dataServe.cardURL;
+        for (const card of shareCardRrss) {
+          card.classList.remove("collapsed");
+        }
+        shareCreationLink.innerHTML = dataServe.cardURL;
+        messageNewCard.innerText = "La tarjeta ha sido creada:";
+        twitterHref.href = `https://twitter.com/intent/tweet?text=%C2%A1Comparte%20esta%20tarjeta%20super%20molona%21&url=${dataServe.cardURL}`;
+        // linkedinHref.href = `https://www.linkedin.com/sharing/share-offsite/?url=${data.cardURL}`;
         //facebookHref.href = `http://www.facebook.com/share.php?u=${data.cardURL}`;
       } else {
-        const messageNewCard = document.querySelector(".share_creation__title");
-        const shareCardRrss = document.querySelectorAll(".js-share-RRSS");
         for (const card of shareCardRrss) {
-          card.classList.add('collapsed');
-          console.log(card);
+          card.classList.add("collapsed");
         }
-        messageNewCard.innerHTML = "¡Error!";
+
+        messageNewCard.innerText = "¡Error!";
         shareCreationLink.innerHTML =
           "Por favor, asegúrese de que ha cumplimentado todos los campos";
       }
@@ -33,10 +37,13 @@ function sendUserInfo() {
 }
 const sharebutton = document.querySelector(".share_button__item");
 const sharecreation = document.querySelector(".share_creation ");
-
+function clearError() {
+  messageNewCard.innerHTML = "";
+}
 //SHARE BUTTON FUCNTION
 function handleButtonClick(event) {
   event.preventDefault();
+
   sendUserInfo();
   changeColor();
 }
